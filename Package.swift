ec3e6740c553a9f42bfea8b556eb09a1ac62a523
Helge Heß <devteam@zeezide.de>
2018-03-23 16:05:42 +0100

    Bump image to Setty 1.3.0
    ... iOS!

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
                  from: "1.0.0"),
         */
         .package(url: "https://github.com/apple/swift-nio.git", 
-                 from: "1.2.1"),
+                 from: "1.3.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", 
                  from: "1.0.0"),
         .package(url: "https://github.com/NozeIO/MicroExpress.git",
