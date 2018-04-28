c12afc78e5b55d49d2b7825b7039aa218a4389b7
Helge Heß <devteam@zeezide.de>
2018-04-28 13:46:28 +0200

    Use NIO 1.5.1

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
                  from: "1.0.0"),
         */
         .package(url: "https://github.com/apple/swift-nio.git", 
-                 from: "1.5.0"),
+                 from: "1.5.1"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", 
                  from: "1.0.0"),
         .package(url: "https://github.com/NozeIO/MicroExpress.git",
