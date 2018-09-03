ffc577d01bffdbd88276457d35a15f0b297b2dbd
Helge Heß <devteam@zeezide.de>
2018-09-03 16:56:00 +0200

    Bump versions of different pkgs, e.g. NIO 1.9.4
    Also use a version for MicroExpress, instead of the
    branch.

--- Package.swift
+++ Package.swift
@@ -9,19 +9,19 @@ let package = Package(
                  from: "1.0.0"),
         */
         .package(url: "https://github.com/apple/swift-nio.git", 
-                 from: "1.8.0"),
+                 from: "1.9.4"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", 
-                 from: "1.0.0"),
+                 from: "1.2.0"),
         .package(url: "https://github.com/NozeIO/MicroExpress.git",
-                 .branch("branches/swift-nio-lib")),
+                 from: "0.4.5"),
         .package(url: "https://github.com/SwiftNIOExtras/swift-nio-redis.git",
                  from: "0.9.0"),
         .package(url: "https://github.com/SwiftNIOExtras/swift-nio-irc.git",
-                 from: "0.6.0"),
+                 from: "0.6.1"),
         .package(url: "https://github.com/NozeIO/swift-nio-redis-client.git",
                  from: "0.9.0"),
         .package(url: "https://github.com/NozeIO/swift-nio-irc-client.git",
-                 from: "0.6.0")
+                 from: "0.6.1")
     ],
     targets: [
         .target(name: "SwiftNIO_XcodeImage", 
