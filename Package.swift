b6ba528f5404a43d2950078fcc08e808b6dfad6e
Helge Heß <devteam@zeezide.de>
2019-03-15 18:54:56 +0100

    Set Package.swift version to 4.2, update pkg versions
    ...

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
@@ -9,15 +9,15 @@ let package = Package(
                  from: "1.0.0"),
         */
         .package(url: "https://github.com/apple/swift-nio.git", 
-                 from: "1.9.4"),
+                 from: "1.13.2"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", 
-                 from: "1.2.0"),
+                 from: "1.4.0"),
         .package(url: "https://github.com/NozeIO/MicroExpress.git",
-                 from: "0.4.5"),
+                 from: "0.4.8"),
         .package(url: "https://github.com/SwiftNIOExtras/swift-nio-redis.git",
                  from: "0.9.0"),
         .package(url: "https://github.com/SwiftNIOExtras/swift-nio-irc.git",
-                 from: "0.6.1"),
+                 from: "0.6.2"),
         .package(url: "https://github.com/NozeIO/swift-nio-redis-client.git",
                  from: "0.9.0"),
         .package(url: "https://github.com/NozeIO/swift-nio-irc-client.git",
